b7cfdb083393328aefc0e8ec0cb33db35c3de4fc
jrogado <jose.rogado@gmail.com>
2022-04-22 00:33:15 +0100

    Added production settings

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express();
 const session = require('express-session');
 
 const passport = require('passport');
+const { createServer } = require('./services/https');
 require('./services/passport');
 
-const {port, https, certs} = require('./services/https');
-
+// const {port, https, certs} = require('./services/https');
+require('./services/https');
 /*  VIEW ENGINE */
 app.set('view engine', 'ejs');
 
@@ -23,6 +24,5 @@ app.use(passport.initialize());
 app.use(passport.session());
 require('./routes/authRoutes')(app);
 
-const https_server = https.createServer(certs, app).listen(port, () => {
-  console.log('HTTPS server listening on: ', https_server.address());
-});
+createServer(app);
+
--- services/https.js
+++ services/https.js
@@ -1,28 +1,44 @@
 /*  HTTPS */
 const https = require("https");
+const http = require('http');
 const fs = require("fs"),
-      dotenv = require('dotenv');
+    dotenv = require('dotenv');
 
 // Get the configuration values
 dotenv.config();
-const port = process.env.PORT;
-var server = process.env.SERVER;
+function createServer(app) {
+    const port = process.env.PORT;
+    if (process.env.NODE_ENV === 'production') {
+        // In production don't use HTTP
+        console.log("Production");
+        const http_server = http.createServer(app).listen(port, () => {
+            console.log('HTTP server listening on: ', http_server.address());
+        });
+    } else {
+        // In development use HTTP
+        console.log("Development");
+        var server = process.env.SERVER;
+        /*  CERTIFICATE */
+        var keyname = "./certs/" + server + "-key.pem";
+        var certname = "./certs/" + server + ".pem";
 
-/*  CERTIFICATE */
-var keyname = "./certs/" + server + "-key.pem";
-var certname = "./certs/" + server + ".pem";
-    
-console.log("Using key:", keyname);
-console.log("Using crt:", certname);
-console.log();
+        console.log("Using key:", keyname);
+        console.log("Using crt:", certname);
+        console.log();
 
-const certs = {
-    key: fs.readFileSync(keyname),
-    cert: fs.readFileSync(certname)
-};
-
-module.exports = {
-    port,
-    https,
-    certs 
+        const certs = {
+            key: fs.readFileSync(keyname),
+            cert: fs.readFileSync(certname)
+        };
+        const https_server = https.createServer(certs, app).listen(port, () => {
+            console.log('HTTPS server listening on: ', https_server.address());
+        });
+    }
 }
+
+module.exports = { createServer }
+// module.exports = {
+//     port,
+//     https,
+//     certs
+// }
