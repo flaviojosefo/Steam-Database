2fe008e272427e4d179051f0766e9f139774b75b
fs000 <flaviosantos002010@hotmail.com>
2022-06-02 03:14:05 +0100

    Add a game adding page and working POST function

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -13,14 +13,18 @@ router.get('/add', /*isAuth,*/ (req, res) => {
 	});
 });
 
-router.post('/add', isAuth, async (req, res) => {
+router.post('/add', /*isAuth, */async (req, res) => {
+	//console.log(req.body);
+	//console.log(formIsValid(req.body));
+	
 	// Create a new game object
 	const newGame = new Game({
-	    name: req.body.name,
+	    title: req.body.title,
 		steamId: req.body.steamId,
 		genres: req.body.genres,
 		developer: req.body.developer,
-		user: req.user.googleId
+		addedBy: getCurrentUser(req),
+		addedAt: getDate().format(new Date())
 	});
 	
 	// Try saving the game to the DB
@@ -34,6 +38,26 @@ router.post('/add', isAuth, async (req, res) => {
             message_tag: 'Failed creating a new Game'
         });
 	}
+	res.redirect(req.originalUrl);
 });
+
+function formIsValid(body) {
+	if(body.title.trim().length == 0) return false;
+	
+	return true;
+}
+
+function getCurrentUser(req) {
+	if (req.isAuthenticated()) {
+		return req.user.googleId;
+	} else {
+		return "Unknown";
+	}
+}
+
+// Returns a date in a specific style
+function getDate() {
+	return new Intl.DateTimeFormat('en-GB', { dateStyle: 'long', timeStyle: 'long' });
+}
 
 module.exports = router;
