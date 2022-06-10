e0d1016125afe27175408a6bb2492a871129af5e
fs000 <flaviosantos002010@hotmail.com>
2022-06-10 18:21:31 +0100

    Refactor most routing code and add comments

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -12,6 +12,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/add', (req, res) => {
+	// Display the 'Add Game' page
 	res.render('add_game', {
 		user: req.user
 	});
@@ -19,53 +20,74 @@ router.get('/add', (req, res) => {
 
 router.get('/store', async (req, res) => {
 	
-	// Get games on store DB and reverse order
-	let storeGames = await Game.find();
-	storeGames = storeGames.reverse();
-	
-	// Get a logo for each game, searching by the game's Id
-	let logos = [];
-	for (let i = 0; i < storeGames.length; i++) {
-		logos.push(await fetchLogo(storeGames[i].steamId));
+	try {
+		// Get games on store DB and reverse order
+		let storeGames = await Game.find();
+		storeGames = storeGames.reverse();
+		
+		// Get a logo for each game, searching by the game's Id
+		let logos = [];
+		for (let i = 0; i < storeGames.length; i++) {
+			logos.push(await fetchLogo(storeGames[i].steamId));
+		}
+		
+		// Array of games owned by user
+		let ownedGames = [];
+		
+		// Check if a user is logged in
+		if (req.isAuthenticated()) {
+			// Retrieve the user's library
+			const userLib = await Library.findOne({ ownderId: req.user.googleId });
+			
+			// Get games on user's library
+			let userGames = userLib.games;
+			
+			// Get user's owned games by steamId
+			userGames.forEach(function(currentValue) { ownedGames.push(currentValue.steamId); });
+		}
+		
+		// Send relevant info to the 'ejs' file
+		res.render('store', {
+			user: req.user,
+			gamesList: storeGames,
+			gameLogos: logos,
+			ownedIds: ownedGames
+		});
+	} catch (err) {
+		// Show the error on the (server's) console
+		console.error(err);
+		// Render an error (client-side)
+		res.render('error', {
+			message_tag: 'Library Error'
+		});
 	}
 	
-	// Array of games owned by user
-	let ownedGames = [];
 	
-	// Check if a user is logged in
-	if (req.isAuthenticated()) {
-		// Retrieve the user's library
-		const userLib = await Library.findOne({ ownderId: req.user.googleId });
-		
-		// Get games on user's library
-		let userGames = userLib.games;
-		
-		// Get user's owned games by steamId
-		userGames.forEach(function(currentValue) { ownedGames.push(currentValue.steamId); });
-	}
-	
-	// Send relevant info to the 'ejs' file
-	res.render('store', {
-		user: req.user,
-		gamesList: storeGames,
-		gameLogos: logos,
-		ownedIds: ownedGames
-	});
 });
 
-router.get('/library', isAuth, hasLib, async (req, res) => {
-	
-	// Fetch the user's library
-	let userLib = await Library.findOne({ ownderId: req.user.googleId });
-	
-	// Check if we get data on a query
-	if (req.query.steamId) {
-		// Check if the game is already owned by the user
-		let alreadyOwned = false;
-		userLib.games.forEach(function(currentValue) { if (currentValue.steamId == req.query.steamId) { alreadyOwned = true; } });
-		console.log('Game ' + req.query.steamId + ' already owned: ' + alreadyOwned);
+router.get('/library', isAuth, async (req, res) => {
+	// Encapsulate code in try/catch to prevent await related errors
+	try {
+		// Fetch a library based on user's googleId
+		let userLib = await Library.findOne({ ownderId: req.user.googleId });
 		
-		try {
+		// Check if user does NOT have a library
+		if (!userLib) {
+			// If not, create a new (empty) one
+			userLib = await new Library({
+				ownderId: req.user.googleId,
+				games: undefined
+			}).save();
+			console.log('Created library for user ' + req.user.name, userLib);	
+		}
+	
+		// Check if we get data on a query
+		if (req.query.steamId) {
+			// Check if the game is already owned by the user
+			let alreadyOwned = false;
+			userLib.games.forEach(function(currentValue) { if (currentValue.steamId == req.query.steamId) { alreadyOwned = true; } });
+			console.log('Game ' + req.query.steamId + ' already owned: ' + alreadyOwned);
+			
 			// Only add a game to the DB if the user doesn't own it
 			if (!alreadyOwned) {
 				// If it does, push the game's 'steamId' to the user's library
@@ -79,51 +101,55 @@ router.get('/library', isAuth, hasLib, async (req, res) => {
 			
 			// Redirect the user to the normal library URL
 			res.redirect(url.parse(req.originalUrl).pathname);
-		} catch (err) {
-			console.error(err);
-			res.render('error', {
-				message_tag: 'Failed adding a Game to the Library'
-			});
+			return;
 		}
-		return;
+		
+		// Get games on user's library and reverse order
+		let userGames = userLib.games;
+		userGames = userGames.reverse();
+		
+		// Get an array with only the steamIds of each game
+		let gamesId = [];
+		userGames.forEach(function(currentValue) { gamesId.push(currentValue.steamId); });
+		//console.log(gamesId);
+		
+		// Fetch games info from store (by steamId)
+		let gamesInfo = await Game.find({ 'steamId': { $in: gamesId } });
+		
+		// Get the positions of each Id on the 'gamesId' array
+		// This accelerates the sort
+		const itemPositions = {};
+		for (const [index, steamId] of gamesId.entries()) {
+			itemPositions[steamId] = index;
+		}
+		
+		// Sort the info array to correspond to the gamesId array
+		gamesInfo.sort((a, b) => itemPositions[a.steamId] - itemPositions[b.steamId]);
+		//console.log(gamesInfo);
+		
+		// Get a logo for each game, searching by the game's steamId
+		// Why not use a forEach? Because forEach doesn't allow 'await'
+		let logos = [];
+		for (let i = 0; i < userGames.length; i++) {
+			logos.push(await fetchLogo(userGames[i].steamId))
+		}
+		
+		// Render the user's library
+		res.render('library', {
+			user: req.user,
+			gamesLibInfo: userGames,
+			ownedGames: gamesInfo,
+			gameLogos: logos
+		});
+		
+	} catch (err) {
+		// Show the error on the (server's) console
+		console.error(err);
+		// Render an error (client-side)
+		res.render('error', {
+			message_tag: 'Library Error'
+		});
 	}
-	
-	// Get games on user's library and reverse order
-	let userGames = userLib.games;
-	userGames = userGames.reverse();
-	
-	// Get an array with the steamIds of each game
-	let gamesId = [];
-	userGames.forEach(function(currentValue) { gamesId.push(currentValue.steamId); });
-	//console.log(gamesId);
-	
-	// Fetch games info from store (by steamId)
-	let gamesInfo = await Game.find({ 'steamId': { $in: gamesId } });
-	
-	// Get the positions of each Id on the 'gamesId' array
-	// This accelerates the sort
-	const itemPositions = {};
-	for (const [index, steamId] of gamesId.entries()) {
-		itemPositions[steamId] = index;
-	}
-	
-	// Sort the info array to correspond to the gamesId array
-	gamesInfo.sort((a, b) => itemPositions[a.steamId] - itemPositions[b.steamId]);
-	//console.log(gamesInfo);
-	
-	// Get a logo for each game, searching by the game's steamId
-	// Why not use forEach here aswell? Because forEach doesn't allow 'await'
-	let logos = [];
-	for (let i = 0; i < userGames.length; i++) {
-		logos.push(await fetchLogo(userGames[i].steamId))
-	}
-	
-	res.render('library', {
-		user: req.user,
-		gamesLibInfo: userGames,
-		ownedGames: gamesInfo,
-		gameLogos: logos
-	});
 });
 
 router.post('/library', async (req, res) => {
@@ -137,12 +163,29 @@ router.post('/library', async (req, res) => {
 			{ ownderId: req.user.googleId }, 
 			{ $pull: { games: { steamId: req.body.removeId } } }
 		);
-	} catch(err) {
-		console.log(err);
+		
+		// Reload the library page
+		res.redirect(req.originalUrl);
+	} catch (err) {
+		// Show the error on the (server's) console
+		console.error(err);
+		// Render an error (client-side)
+		res.render('error', {
+			message_tag: 'Could not remove game with id ' + req.body.removeId
+		});
+	}
+});
+
+router.get('/library/:id', (req, res) => {
+	
+	try {
+		
+	} catch (err) {
+		
 	}
 	
-	// Reload the library page
-	res.redirect(req.originalUrl);
+	//const externalInfo = await getSteamInfo('1085660');
+	//console.log(externalInfo.success);
 });
 
 router.get('/store/:id', (req, res) => {
@@ -150,38 +193,52 @@ router.get('/store/:id', (req, res) => {
 	console.log(req.params.id);
 	res.redirect('/games/store');
 	
+	// Display game info
+	
 	//const externalInfo = await getSteamInfo('1085660');
 	//console.log(externalInfo.success);
 });
 
 router.post('/add', async (req, res) => {
-	//console.log(req.body);
-	//console.log(formIsValid(req.body));
-	
-	// Create a new game object
-	const newGame = new Game({
-	    title: req.body.title,
-		steamId: req.body.steamId,
-		genres: req.body.genres,
-		developer: req.body.developer,
-		addedBy: getCurrentUser(req)[0],
-		addedById: getCurrentUser(req)[1],
-		addedAt: getDate().format(new Date())
-	});
 	
 	// Try saving the game to the DB
 	try {
+		// Get a game with the declared steamId
+		let gameToAdd = await Game.findOne({ steamId: req.body.steamId });
+		
+		// Check if the game already exists
+		if (gameToAdd) {
+			// Render 'add' page and show message
+		} else {
+			// Create game and redirect to the store
+		}
+		
+		// Create a new 'game' object
+		const newGame = new Game({
+			title: req.body.title,
+			steamId: req.body.steamId,
+			genres: req.body.genres,
+			developer: req.body.developer,
+			addedBy: getCurrentUser(req)[0],
+			addedById: getCurrentUser(req)[1],
+			addedAt: getDate().format(new Date())
+		});
+		
+		// Attempt to save the game on the DB
 		const thisGame = await newGame.save();
 		console.log('Added game:', newGame);
-		//res.redirect('/display_library');
+		
+		// Redirect to the 'store' page
+		res.redirect('/games/store');
+		
 	} catch (err) {
+		// Show the error on the (server's) console
 		console.error(err);
+		// Render an error (client-side)
 		res.render('error', {
             message_tag: 'Failed creating a new Game'
         });
 	}
-	
-	res.redirect('/games/store');
 });
 
 async function getSteamInfo(appId) {
@@ -211,28 +268,31 @@ async function getSteamInfo(appId) {
 async function fetchLogo(appId) {
 	
 	// Default URL for a game's logo
-	const logoUrl = "https://cdn.akamai.steamstatic.com/steam/apps/" + appId + "/capsule_231x87.jpg";
+	const logoUrl = 'https://cdn.akamai.steamstatic.com/steam/apps/' + appId + '/capsule_231x87.jpg';
 	
 	// Fetch logo URL
 	const response = await fetch(logoUrl);
 	
 	// Check if URL returns empty (e.g.: 404)
 	if (response.ok) {
-		//console.log('logo found!');
-		// Return an external logo
+		// If url is found, return an external logo
 		return logoUrl;
 	} else {
-		//console.log('logo NOT found!');
-		// Return a default logo in the server
-		return "/images/applogo.svg";
+		// If url is not found, return a default logo in the server's files
+		return '/images/applogo.svg';
 	}
 }
 
+// Get the user's name and Id
+// for game creation purposes
 function getCurrentUser(req) {
+	// Check if user is authenticated
 	if (req.isAuthenticated()) {
+		// If he is, return his name and googleId
 		return [req.user.name, req.user.googleId];
 	} else {
-		return ["Unknown", 0];
+		// If not he isn't, return 'unknown' user
+		return ['Unknown', 0];
 	}
 }
 
